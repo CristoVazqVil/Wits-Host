468177eded5bc68e3907008c933d808cd697cc98
CrisVazV <cris990077@hotmail.com>
2024-01-05 01:12:07 -0600

    Tests :3

--- WitsClassesTests/PlayerManagerTests.cs
+++ WitsClassesTests/PlayerManagerTests.cs
@@ -53,6 +53,27 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
+        public void AddPlayerFailedInvalid()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            Player newPlayer = new Player();
+            newPlayer.Username = null;
+            newPlayer.Email = null;
+            newPlayer.UserPassword = null;
+            newPlayer.HighestScore = 0;
+            newPlayer.ProfilePictureId = 0;
+            newPlayer.CelebrationId = 0;
+
+            // Act
+            int result = manager.AddPlayer(newPlayer);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
         public void AddPlayerException()
         {
             // Arrange
@@ -104,6 +125,21 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
+        public void DeletePlayerFailedInvalid()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string playerToDelete = null;
+
+            // Act
+            int result = manager.DeletePlayer(playerToDelete);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
         public void DeletePlayerException()
         {
             // Arrange
@@ -146,6 +182,21 @@ namespace WitsClassesTests
             PlayerManager manager = new PlayerManager();
             Player expectedPlayer = null;
             string playerToGet = "NotExistingPlayer";
+
+            // Act
+            Player result = manager.GetPlayerByUser(playerToGet);
+
+            // Assert
+            Assert.AreEqual(expectedPlayer, result);
+        }
+
+        [TestMethod]
+        public void GetPlayerByUserFailedInvalid()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            Player expectedPlayer = null;
+            string playerToGet = null;
 
             // Act
             Player result = manager.GetPlayerByUser(playerToGet);
@@ -208,6 +259,38 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
+        public void GetPlayerByUserAndPasswordFailedWrongPassword()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            Player expectedPlayer = null;
+            string playerToGet = "CrisCris";
+            string userPassword = "password";
+
+            // Act
+            Player result = manager.GetPlayerByUserAndPassword(playerToGet, userPassword);
+
+            // Assert
+            Assert.AreEqual(expectedPlayer, result);
+        }
+
+        [TestMethod]
+        public void GetPlayerByUserAndPasswordFailedWrongUser()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            Player expectedPlayer = null;
+            string playerToGet = "UnitTest78";
+            string userPassword = "Criscris9?";
+
+            // Act
+            Player result = manager.GetPlayerByUserAndPassword(playerToGet, userPassword);
+
+            // Assert
+            Assert.AreEqual(expectedPlayer, result);
+        }
+
+        [TestMethod]
         public void GetPlayerByUserAndPasswordException()
         {
             // Arrange
@@ -256,6 +339,22 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
+        public void UpdatePasswordFailedInvalid()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string username = null;
+            string passwordToUpdate = "password";
+
+            // Act
+            int result = manager.UpdatePassword(username, passwordToUpdate);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
         public void UpdatePasswordException()
         {
             // Arrange
@@ -288,13 +387,45 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void AddRequestFailed()
+        public void AddRequestFailedBothNotExisting()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             int expected = 0;
-            string from = "UnitTest";
+            string from = "UnitTest000";
             string to = "UnitTest452";
+
+            // Act
+            int result = manager.AddRequest(from, to);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AddRequestFailedFromNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string from = "UnitTest000";
+            string to = "CrisCris";
+
+            // Act
+            int result = manager.AddRequest(from, to);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AddRequestFailedToNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string from = "CrisCris";
+            string to = "UnitTest452666";
 
             // Act
             int result = manager.AddRequest(from, to);
@@ -384,13 +515,61 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void AcceptRequestFailed()
+        public void AcceptRequestFailedNotExistingRequest()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string receiver = "UnitTest";
+            string sender = "CrisCris";
+
+            // Act
+            int result = manager.AcceptRequest(receiver, sender);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AcceptRequestFailedBothNotExisting()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             int expected = 0;
             string receiver = "UnitTest256";
             string sender = "UnitTest45";
+
+            // Act
+            int result = manager.AcceptRequest(receiver, sender);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AcceptRequestFailedReceiverNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string receiver = "UnitTest256000";
+            string sender = "CrisCris";
+
+            // Act
+            int result = manager.AcceptRequest(receiver, sender);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AcceptRequestFailedSenderNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string receiver = "CrisCris";
+            string sender = "UnitTest467465";
 
             // Act
             int result = manager.AcceptRequest(receiver, sender);
@@ -432,13 +611,61 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void RejectRequestFailed()
+        public void RejectRequestFailedNotExistingRequest()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string receiver = "CrisCris";
+            string sender = "UnitTest";
+
+            // Act
+            int result = manager.RejectRequest(receiver, sender);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void RejectRequestFailedBothNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string receiver = "UnitTest27456";
+            string sender = "UnitTest45676";
+
+            // Act
+            int result = manager.RejectRequest(receiver, sender);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void RejectRequestFailedReceiverNotExisting()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             int expected = 0;
             string receiver = "UnitTest256";
-            string sender = "UnitTest45";
+            string sender = "CrisCris";
+
+            // Act
+            int result = manager.RejectRequest(receiver, sender);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void RejectRequestFailedSenderNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string receiver = "CrisCris";
+            string sender = "Unit453903";
 
             // Act
             int result = manager.RejectRequest(receiver, sender);
@@ -481,13 +708,64 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void DeleteRequestFailed()
+        public void DeleteRequestFailedNotExistingRequest()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             int expected = 0;
             string sender = "UnitTest";
             string receiver = "CrisCris";
+            int status = 1;
+
+            // Act
+            int result = manager.DeleteRequest(receiver, sender, status);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeleteRequestFailedBothNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string sender = "Test46585";
+            string receiver = "test583402";
+            int status = 1;
+
+            // Act
+            int result = manager.DeleteRequest(receiver, sender, status);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeleteRequestFailedSenderNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string sender = "test54763";
+            string receiver = "CrisCris";
+            int status = 1;
+
+            // Act
+            int result = manager.DeleteRequest(receiver, sender, status);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeleteRequestFailedReceiverNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string sender = "UnitTest";
+            string receiver = "test38483";
             int status = 1;
 
             // Act
@@ -531,12 +809,44 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void AddFriendshipFailed()
+        public void AddFriendshipFailedBothNotExisting()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             int expected = 0;
             string player = "NotExisting";
+            string friend = "NotExisting2";
+
+            // Act
+            int result = manager.AddFriendship(player, friend);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AddFriendshipFailedPlayerNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string player = "NotExisting";
+            string friend = "CrisCris";
+
+            // Act
+            int result = manager.AddFriendship(player, friend);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void AddFriendshipFailedFriendNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string player = "CrisCris";
             string friend = "NotExisting2";
 
             // Act
@@ -579,13 +889,61 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void DeleteFriendshipFailed()
+        public void DeleteFriendshipFailedNotExistingFriendship()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             int expected = 0;
-            string player = "UnitTest678";
+            string player = "CrisCris";
+            string friend = "UnitTest";
+
+            // Act
+            int result = manager.DeleteFriendship(player, friend);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeleteFriendshipFailedPlayerNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string player = "UnitTest675658";
+            string friend = "CrisCris";
+
+            // Act
+            int result = manager.DeleteFriendship(player, friend);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeleteFriendshipFailedFriendNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string player = "CrisCris";
             string friend = "UnitTest8947";
+
+            // Act
+            int result = manager.DeleteFriendship(player, friend);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void DeleteFriendshipFailedBothNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string player = "UnitTest6tt78";
+            string friend = "UnitTest894tt7";
 
             // Act
             int result = manager.DeleteFriendship(player, friend);
@@ -627,12 +985,44 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void BlockPlayerFailed()
+        public void BlockPlayerFailedBothNotExisting()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             int expected = 0;
             string player = "NotExisting";
+            string playerToBlock = "NotExisting2";
+
+            // Act
+            int result = manager.BlockPlayer(player, playerToBlock);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void BlockPlayerFailedPlayerNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string player = "NotExisting";
+            string playerToBlock = "CrisCris";
+
+            // Act
+            int result = manager.BlockPlayer(player, playerToBlock);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void BlockPlayerFailedPlayerToBlockNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            int expected = 0;
+            string player = "CrisCris";
             string playerToBlock = "NotExisting2";
 
             // Act
@@ -675,12 +1065,44 @@ namespace WitsClassesTests
         }
 
         [TestMethod]
-        public void IsPlayerBlockedFailed()
+        public void IsPlayerBlockedFailedBothNotExisting()
         {
             // Arrange
             PlayerManager manager = new PlayerManager();
             bool expected = false;
             string player = "NotExisting";
+            string blockedPlayer = "NotExisting45";
+
+            // Act
+            bool result = manager.IsPlayerBlocked(player, blockedPlayer);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void IsPlayerBlockedFailedPlayerNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            bool expected = false;
+            string player = "NotExisting";
+            string blockedPlayer = "CrisCris";
+
+            // Act
+            bool result = manager.IsPlayerBlocked(player, blockedPlayer);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void IsPlayerBlockedFailedBlockedPlayerNotExisting()
+        {
+            // Arrange
+            PlayerManager manager = new PlayerManager();
+            bool expected = false;
+            string player = "CrisCris";
             string blockedPlayer = "NotExisting45";
 
             // Act
